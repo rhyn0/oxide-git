ef19f159c06d00ab7c33121f0d53b63023c02f6b
Ryan Ozawa <ryan.ozawa21@gmail.com>
2024-02-28 23:54:37 -0800

    handle .ignore files crudely on first pass

--- src/data/base.rs
+++ src/data/base.rs
@@ -1,4 +1,7 @@
-use std::{fs, path::PathBuf};
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
 
 /// Higher order operations on data.
 use crate::data::objects::{OgitObject, OgitObjectType};
@@ -14,6 +17,9 @@ pub fn write_tree(directory: Option<PathBuf>) -> Result<OgitObject, std::io::Err
         // take it straight fromo the examples
         let entry = entry?;
         let path = entry.path();
+        if is_ignored(&path) {
+            continue;
+        }
         if path.is_dir() {
             let sub_tree = write_tree(Some(path))?;
             println!("sub_tree: {sub_tree:?}");
@@ -22,4 +28,10 @@ pub fn write_tree(directory: Option<PathBuf>) -> Result<OgitObject, std::io::Err
         }
     }
     Ok(OgitObject::new("tree", OgitObjectType::Tree))
+}
+
+/// Returns whether a file is ignored or not.
+pub fn is_ignored(path: &Path) -> bool {
+    // for now we only ignore the .ogit directory
+    path.components().any(|c| c.as_os_str() == ".ogit")
 }
